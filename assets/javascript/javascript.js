ecadb3e819550fea65ec9bd745e48be6aad60e15
unknown <yutaatamura@gmail.com>
2018-03-21 23:33:29 -0500

    Add event listener for user input and store input in variable. AJAX call for default most popular posts on page load. AJAX call for query input on keypress event. Add ID tags with contentIndex to each card to iterate through JSON results.

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -5,13 +5,88 @@
 // search bar (default= Title order by popularity on search)
 // 1. event listener for user input (input validation)
     //Event listener
+    var contentIndex = 0;
+    var queryInput = "";
+    $("#search").keypress(function(e) {
+        if (e.which == 13) {
+            event.preventDefault();
+            queryInput = $("#search").val().trim();
+            console.log(queryInput);
+            $("#search").val('');
+            contentIndex = 0;
+            
+        
     // Ajax parameter (q=) variable 
 // 2. (a)store user input in varaiable.
 //    (b)send search term to database
+var url = "https://api.nytimes.com/svc/movies/v2/reviews/search.json";
+            url += '?' + $.param({
+                'api-key': "386607bb9b9e4ed39bea6265563b90a3",
+                'query': queryInput
+            });
+            $.ajax({
+                url: url,
+                method: 'GET',
+            })
+                // We store all of the retrieved data inside of an object called "response"
+                .then(function (result) {
+                    // Log the resulting object
+                    console.log(result);
+                    for (var i = 0; i < result.results.length; i++) {
+                        // Transfer content to HTML
+                        $("#title"+contentIndex+"").html(result.results[i].display_title);
+                        // $(".title").text("Title: " + result.results[0].headline);
+                        $("#content"+contentIndex+"").html(result.results[i].summary_short);
+
+                        //return the default library picture if no multimedia exists in JSON object
+                        if (result.results[i].multimedia === null) {
+                            console.log("no multimedia exists");
+                        $("#cardImg"+contentIndex+"").attr("src","./assets/images/bookstore-slide-2MCD-superJumbo.jpg");
+
+                        } else {
+
+                            $("#cardImg"+contentIndex+"").attr("src", result.results[i].multimedia.src);
+                        }
+                        // $(".snippet").text("Article Snippet: " + result.results[0].summary_short);
+                        // $(".pubDate").text("Rating: " + result.results[0].mpaa_rating);
+
+                        contentIndex++;
+                        console.log(contentIndex);
+                    }
+                });
+                }
+            });
 
 
+            //ajax call for default search items (most popular posts regarding movies) on page load
 
+            $(document).ready(function() {
 
+            var url = "https://api.nytimes.com/svc/mostpopular/v2/mostviewed/Movies/1.json";
+                url += '?' + $.param({
+                'api-key': "386607bb9b9e4ed39bea6265563b90a3"
+                });
+                $.ajax({
+                url: url,
+                method: 'GET',
+                })
+
+                .then(function (result) {
+                    // Log the resulting object
+                    console.log(result);
+                    for (var i = 0; i < result.results.length; i++) {
+                        // Transfer content to HTML
+                        $("#title"+contentIndex+"").text(result.results[i].title);
+                      
+                        $("#content"+contentIndex+"").text(result.results[i].abstract);
+                        
+                        $("#cardImg"+contentIndex+"").attr("src", result.results[i].media["0"]["media-metadata"][1].url);
+                        
+
+                        contentIndex++;
+                    }
+                });
+            });
 // B. TABS for different media
 // dropdown search parameter(filter with radio buttons options). onclick show/hide table
         // Event listener (for each filter)
