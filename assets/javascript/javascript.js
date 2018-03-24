257d71e43b76521c29fa7aaa82e2c3b5bb99cf43
Yuta <yutaatamura@gmail.com>
2018-03-24 11:15:08 -0500

    Create new file for NYT AJAX call for details page

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -20,48 +20,6 @@
                                 // 2. (a)store user input in varaiable.
                                 //    (b)send search term to database
 
-//--------------------THIS IS THE NYT VERSION OF CODE----------------
-// var url = "https://api.nytimes.com/svc/movies/v2/reviews/search.json";
-//             url += '?' + $.param({
-//                 'api-key': "386607bb9b9e4ed39bea6265563b90a3",
-//                 'query': queryInput
-//             });
-//             $.ajax({
-//                 url: url,
-//                 method: 'GET',
-//             })
-//                                 // We store all of the retrieved data inside of an object called "response"
-//                 .then(function (result) {
-//                                 // Log the resulting object
-//                     console.log(result);
-//                     for (var i = 0; i < result.results.length; i++) {
-//                                 // Transfer content to HTML
-//                         $("#title"+contentIndex+"").html(result.results[i].display_title);
-//                                 // $(".title").text("Title: " + result.results[0].headline);
-//                         $("#content"+contentIndex+"").html(result.results[i].summary_short);
-
-//                                 // return the default library picture if no multimedia exists in JSON object
-//                         if (result.results[i].multimedia === null) {
-//                             console.log("no multimedia exists");
-//                         $("#cardImg"+contentIndex+"").attr("src","./assets/images/bookstore-slide-2MCD-superJumbo.jpg");
-
-//                         } else {
-
-//                             $("#cardImg"+contentIndex+"").attr("src", result.results[i].multimedia.src);
-//                         }
-//                                 // $(".snippet").text("Article Snippet: " + result.results[0].summary_short);
-//                                 // $(".pubDate").text("Rating: " + result.results[0].mpaa_rating);
-
-//                         contentIndex++;
-//                         console.log(contentIndex);
-//                     }
-//                 });
-//                 }
-//             });
-
-
-                    
-//--------------------------------------------------------------
 $(document).ready(function() {
 //function that renders buttons for years from 1930-2018
 
@@ -208,7 +166,7 @@ if (e.which == 13) { // When enter is pressed fire function
                 // };
 
                 // Genre Filter
-
+                
                     // Event listener for each button that responds to ID
                     // Iterate through genre IDs - dataAccess.media_types[i]
                     // If for each ID that compares to genre_ids of json object
