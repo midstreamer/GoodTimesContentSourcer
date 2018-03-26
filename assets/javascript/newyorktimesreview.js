9316a2e5f66d406af4ee211106711acef3ce5412
Yuta Tamura <ytamura@shin-tech.com>
2018-03-26 11:57:10 -0500

    integrate youtube api with query input

--- assets/javascript/newyorktimesreview.js
+++ assets/javascript/newyorktimesreview.js
@@ -13,13 +13,148 @@ firebase.initializeApp(config);
 
 var loadData = firebase.database(); 
 
+//     contentIndex = 0;
+//     var queryInput = "Dunkirk";
+
+// var url = "https://api.nytimes.com/svc/movies/v2/reviews/search.json";
+//              url += '?' + $.param({
+//                  'api-key': "386607bb9b9e4ed39bea6265563b90a3",
+//                  'query': queryInput
+//              });
+//              $.ajax({
+//                  url: url,
+//                  method: 'GET',
+//              })
+//                                  // We store all of the retrieved data inside of an object called "response"
+//                  .then(function (result) {
+//                                  // Log the resulting object
+//                      console.log("i am result", result);
+//                      for (var i = 0; i < result.results.length; i++) {
+//                                  // Transfer content to HTML
+//                          $("#NytTitle"+contentIndex+"").html(result.results[i].headline);
+//                                  // $(".title").text("Title: " + result.results[0].headline);
+//                          $("#NytContent"+contentIndex+"").html(result.results[i].summary_short);
+
+//                                  // return the default library picture if no multimedia exists in JSON object
+//                          if (result.results[i].multimedia === null) {
+//                              console.log("no multimedia exists");
+//                          $("#NytImg"+contentIndex+"").attr("src","./assets/images/bookstore-slide-2MCD-superJumbo.jpg");
+
+//                          } else {
+
+//                              $("#NytImg"+contentIndex+"").attr("src", result.results[i].multimedia.src);
+//                          }
+//                                  // $(".snippet").text("Article Snippet: " + result.results[0].summary_short);
+//                                  // $(".pubDate").text("Rating: " + result.results[0].mpaa_rating);
+
+//                          contentIndex++;
+//                          console.log(contentIndex);
+//                      }
+//                     });
+// youtube API
+    
+    // var URL = "https://www.googleapis.com/youtube/v3/search?"
+    // var part = "&part=snippet&q=" //snippet is the default setting, but it gives us the information that we looking for 
+    // var trailers = "trailers"
+    // var q = queryInput +" "+ trailers //searching for music, we will change this with the uer input below
+    // var results = "&maxResults=5" //results can be 0 to 50 
+    // var order = "&order=relevance" //listed in order of relevance 
+    // var type = "&type=video"  //this has to be (channel, playlist, or video)
+    // var videoSyndicated = "&videoSyndicated=true" //if true this only returns videos that can be played outside of youtube 
+    // var videoEmbeddable = "&videoEmbeddable=true" //only returns embeddable videos
+    // var videoLicense = "&videoLicense=creativeCommon" //only returns videos with a creative common license
+    // // var videoCategory = "&videoCategoryId=30" //category 30 is movies 
+    // var APIkey = "&key=AIzaSyBP43ErpwhWXmxgZehpzg3e0obiYKKNHfY";
+    // var queryURL = URL + part + q + results + order + type + videoSyndicated + videoEmbeddable + videoLicense + APIkey
+
+    // // https://www.googleapis.com/youtube/v3/search?&part=snippet&maxResults=30
+    // // &order=relevance&type=video&videoCaption=any&videoCategoryId=10
+    // // &key=AIzaSyBP43ErpwhWXmxgZehpzg3e0obiYKKNHfY
+
+    // // Creating an AJAX call for the specific button being clicked
+    // $.ajax({
+    //     url: queryURL,
+    //     method: "GET"
+    // }).then(function (response) {
+    //     console.log(response);
+    //     for (var i = 0; i < response.items.length; i++){
+    //     console.log(response.items[i].id.videoId);
+    //     console.log(response.items[i].snippet.title);
+    //     console.log(response.items[i].snippet.description);
+    //     console.log(response.items[i].snippet.thumbnails.high);
+
+    //     // Storing the link to the video on YouTube
+    //     var youTubeLink = "https://www.youtube.com/watch?v=" + response.items[0].id.videoId;
+
+    //     $("#trailerBtn").attr("href", youTubeLink);
+
+    //     // // Creating an element to hold the link to the video on YouTube
+    //     // var pTwo = $("<p>").text("YouTube Link: " + youTubeLink);
+
+    //     // // Displaying the YouTube Link
+    //     // movieDiv.append(pTwo);
+
+    //     // // Storing the plot or description
+    //     // var plot = response.items[i].snippet.description;
+
+    //     // // Creating an element to hold the plot or description
+    //     // var pThree = $("<p>").text("Description: " + plot);
+
+    //     // // Appending the plot or description
+    //     // movieDiv.append(pThree);
+
+    //     // // Retrieving the URL for the image
+    //     // var imgURL = response.items[i].snippet.thumbnails.high.url;
+
+    //     // // Creating an element to hold the image
+    //     // var image = $("<img>").attr("src", imgURL);
+
+    //     // // Appending the image
+    //     // movieDiv.append(image);
+
+    //     // // Putting the entire movie above the previous movies
+    //     // $("#movies-view").prepend(movieDiv);
+    //     }
+    // });
+    
+    var cardval;
+    loadData.ref('cardvalue').once('value').then(function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+            cardval = childSnapshot.val();
+            console.log("cardval: "+cardval);
+        })    
+        });
+
+    //POPULATE TITLE TO INFO PAGE
+    var titlekeyArray;  
+    var titleIndex;   
+    loadData.ref('KEYtitlekeyarray').once('value').then(function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+            titlekeyArray = childSnapshot.val();
+            console.log("titlekeyarray: "+titlekeyArray);
+            titleIndex = titlekeyArray[cardval];
+            console.log("titleindex= "+titleIndex)
+        })    
+        });
+
+    var titleOfInterest;
+    var rootRef = firebase.database().ref();
+    var urlRef = rootRef.child("title");
+    urlRef.once("value", function(snapshot) {
+    snapshot.forEach(function(child) {
+    if (child.key === titleIndex) {
+    titleOfInterest = child.val();
+    console.log(titleOfInterest);
+    console.log(typeof titleOfInterest)
+    $("#sd-bar-title").text(titleOfInterest);
+
     contentIndex = 0;
     var queryInput = "Dunkirk";
 
 var url = "https://api.nytimes.com/svc/movies/v2/reviews/search.json";
              url += '?' + $.param({
                  'api-key': "386607bb9b9e4ed39bea6265563b90a3",
-                 'query': queryInput
+                 'query': titleOfInterest
              });
              $.ajax({
                  url: url,
@@ -51,12 +186,10 @@ var url = "https://api.nytimes.com/svc/movies/v2/reviews/search.json";
                          console.log(contentIndex);
                      }
                     });
-// youtube API
-    
-    var URL = "https://www.googleapis.com/youtube/v3/search?"
+                    var URL = "https://www.googleapis.com/youtube/v3/search?"
     var part = "&part=snippet&q=" //snippet is the default setting, but it gives us the information that we looking for 
     var trailers = "trailers"
-    var q = queryInput +" "+ trailers //searching for music, we will change this with the uer input below
+    var q = titleOfInterest +" "+ trailers //searching for music, we will change this with the uer input below
     var results = "&maxResults=5" //results can be 0 to 50 
     var order = "&order=relevance" //listed in order of relevance 
     var type = "&type=video"  //this has to be (channel, playlist, or video)
@@ -116,37 +249,6 @@ var url = "https://api.nytimes.com/svc/movies/v2/reviews/search.json";
         // $("#movies-view").prepend(movieDiv);
         }
     });
-    
-    var cardval;
-    loadData.ref('cardvalue').once('value').then(function(snapshot) {
-        snapshot.forEach(function(childSnapshot) {
-            cardval = childSnapshot.val();
-            console.log("cardval: "+cardval);
-        })    
-        });
-
-    //POPULATE TITLE TO INFO PAGE
-    var titlekeyArray;  
-    var titleIndex;   
-    loadData.ref('KEYtitlekeyarray').once('value').then(function(snapshot) {
-        snapshot.forEach(function(childSnapshot) {
-            titlekeyArray = childSnapshot.val();
-            console.log("titlekeyarray: "+titlekeyArray);
-            titleIndex = titlekeyArray[cardval];
-            console.log("titleindex= "+titleIndex)
-        })    
-        });
-
-    var titleOfInterest;
-    var rootRef = firebase.database().ref();
-    var urlRef = rootRef.child("title");
-    urlRef.once("value", function(snapshot) {
-    snapshot.forEach(function(child) {
-    if (child.key === titleIndex) {
-    titleOfInterest = child.val();
-    console.log(titleOfInterest);
-    console.log(typeof titleOfInterest)
-    $("#sd-bar-title").text(titleOfInterest);
     }
   }); 
 });
